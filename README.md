591f5d0aa4fe4d475b4383ff4f86cfe5cbfb64b1
André Carvalho <afacarvalho@yahoo.com.br>
2019-01-25 17:47:41 -0200

    Starting the implementation of the Temporal Dimension Control for WMS Layers tool.

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # Simple WMS leaflet
 Leaflet - Simple WMS Example
 
+Tests with Time Dimension.
+Putting a temporal slider to control display layers with temporal dimension.
+
 # Thanks to:
 
 Jquery Loading: http://carlosbonetti.github.io/jquery-loading
+SOCIB public code: https://github.com/socib/Leaflet.TimeDimension
--- js/map.js
+++ js/map.js
@@ -6,6 +6,8 @@ var host_api = "http://terrabrasilis2.dpi.inpe.br:13003/api/v1";
 let baseLayers = {};
 let overLayers = {};
 
+let hasTemporalLayers=false;
+
 $( document ).ready(function() {
     $("body").loading();
 
@@ -18,6 +20,7 @@ function loadMapOnly() {
         fullscreenControl: {
             pseudoFullscreen: false
         },
+        timeDimension: true
     }).setView([longitude, latitude], 5);
 
     loadBaseLayers();
@@ -28,7 +31,7 @@ function loadBaseLayers() {
     $.ajax({
         url: api,
         async: true,        
-    }).done(function (data) { 
+    }).done(function (data) {
         let baselayers = new Array();
         let overlayers = new Array();
 
@@ -40,8 +43,18 @@ function loadBaseLayers() {
                     overlayers.push(l);
             });
 
+            // Create and insert a new overlayer with temporal dimension enabled to test
+            var lJson="{'id':'5c49f5901a21020001cd6637','name':'temporal_mosaic','title':'LANDSAT 2000/2018','description':'LANDSAT 2000/2018',"+
+            "'attribution':'','workspace':'prodes-cerrado','capabilitiesUrl':'REQUEST=GetCapabilities&VERSION=1.3.0&SERVICE=wms','stackOrder':1,"+
+            "'opacity':1,'baselayer':false,'active':true,'enabled':true,'created':'2019-01-24 17:27:43','datasource':{'id':'5c409e920e9b2a0b8424ef1b',"+
+            "'name':'Prodes Cerrado','description':'Prodes Cerrado','host':'http://terrabrasilis.dpi.inpe.br/geoserver/ows','metadata':'','enabled':true,"+
+            "'created':'2019-01-17 15:26:10','downloads':[],'tools':[]},'tools':[],'subdomains':[]}";
+            var tdLayer=JSON.parse(lJson.replace(/'/g,'"'));
+            overlayers.push(tdLayer);
+
             e.visions.forEach(e => {
                 e.layers.forEach(l => {
+                    l.active=false;
                     if(l.baselayer)
                         baselayers.push(l);
                     else
@@ -63,19 +76,36 @@ function loadBaseLayers() {
             });
             
             if (l.active)
-                baseLayers[l.title].addTo(map);          
+                baseLayers[l.title].addTo(map);
         });
         baseLayers['Empty'] = L.tileLayer(''); 
 
         overlayers.forEach(l => {
-            let url = l.datasource.host.replace("ows", "gwc/service/wms");
 
-            overLayers[l.title] = L.tileLayer.wms(url, {
+            let url = (l.capabilitiesUrl && l.capabilitiesUrl!="")?(l.datasource.host):(l.datasource.host.replace("ows", "gwc/service/wms"));
+
+            var leafletLayer = L.tileLayer.wms(url, {
                 layers: l.workspace + ':' + l.name,
                 format: 'image/png',
                 transparent: true,
-                tiled: true
+                tiled: true,
+                attribution: 'INPE/OBT/DPI/TerraBrasilis'
             });
+
+            if(l.capabilitiesUrl && l.capabilitiesUrl!="") {
+                hasTemporalLayers=true;// to control the enable or disable TimeDimension component
+                var tdOptions={
+                    requestTimeFromCapabilities: true,
+                    getcapabilitiesUrl: l.datasource.host,
+                    setDefaultTime: true,
+                    wmsVersion: "1.3.0"
+                };
+                overLayers[l.title] = L.timeDimension.layer.wms(leafletLayer,tdOptions);
+                // If any layer have a Time Dimension so we add the timedimension component to the map
+                // L.TimeDimension().addTo(map);
+            }else{
+                overLayers[l.title] = leafletLayer;
+            }
 
             if (l.active)
                 overLayers[l.title].addTo(map); 
@@ -83,7 +113,7 @@ function loadBaseLayers() {
 
         loadMapControllers();
     });
-} 
+}
 
 function loadMapControllers() {
     var options = {
@@ -100,6 +130,14 @@ function loadMapControllers() {
      * Scale tool
      */
     L.control.scale().addTo(map);
+
+
+    if(hasTemporalLayers) {
+        L.control.timeDimension({
+            timeInterval: "P1Y/"+(new Date()).toISOString(),
+            period: "P1Y"
+        }).addTo(map);
+    }
 
     /**
      * Stop the animation
